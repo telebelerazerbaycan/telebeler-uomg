e48b4b175832c0c78f6ea1be80fea03f79fc7ebd
Tələbələr Azərbaycan <elmidarqarayev319@gmail.com>
2025-02-16 21:02:49 +0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -7,19 +7,6 @@ mobilSideBar.addEventListener('click', function(){
 });
 
 // Swiper
-document.addEventListener('DOMContentLoaded', function(){
-    const swiper = new Swiper('mySwiper', {
-        loop:true,
-        autoplay:{
-            delay:2000,
-            disableOnInteraction: false
-        },
-        effect:'slide',
-        navigation:{
-            nextEl:'.swiper-button-next',
-            prevEl:'.swiper-button-prev',
-        },
-    });
-});
+
 
 
