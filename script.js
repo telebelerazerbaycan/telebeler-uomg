651569bc463ab48ec7ace9e77966310e735e94c5
Tələbələr Azərbaycan <elmidarqarayev319@gmail.com>
2026-01-31 15:44:25 +0400

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ bars.addEventListener('click', ()=>{
 close.addEventListener('click', ()=>{
     hamburgerMenu.classList.remove('opens')
 });
+
+/// Loading ekranı
+const loadingTime = 2; // saniyə (istədiyini yaz: 3,5,8,10)
+
+let progress = 0;
+const bar = document.getElementById("progress-bar");
+const percentText = document.getElementById("percent");
+
+const intervalTime = (loadingTime * 1000) / 100; // hər 1% üçün vaxt
+
+let timer = setInterval(() => {
+  progress++;
+  bar.style.width = progress + "%";
+  percentText.innerText = progress + "%";
+
+  if(progress >= 100){
+    clearInterval(timer);
+    document.getElementById("loading").style.display = "none";
+  }
+}, intervalTime);
